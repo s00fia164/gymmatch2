f368ba2af48739a03e512a30677f32a4afaea55a
s00fia164 <sonyazelenkova2007@gmail.com>
2024-10-04 13:32:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Prosty algorytm symulujący "swipe left" i "swipe right"
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.swiper-container img');
 
+document.getElementById('like').addEventListener('click', function() {
+    swipeImage('like');
+});
+
+document.getElementById('dislike').addEventListener('click', function() {
+    swipeImage('dislike');
+});
+
+function swipeImage(action) {
+    images[currentImageIndex].style.display = 'none';
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.display = 'block';
+}
